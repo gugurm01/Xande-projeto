9f962a69ccd6df66fdf674bd1565aea1da85eb32
Gustavo de Medeiros <143119460+gugurm01@users.noreply.github.com>
2024-03-06 13:07:03 -0300

    Atualização

---
+++ xande/Assets/Assets/Scripts/PlayerMoviment.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    Rigidbody2D body;
+    public float speed = 5;
+    float horizontal;
+    float vertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        body.velocity = new Vector2(horizontal * speed, vertical * speed);
+    }
+}
---
+++ xande/Assets/Assets/Scripts/XP.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XP : MonoBehaviour
+{
+    public int xpGain;
+    Rigidbody2D body;
+    Transform posPlayer;
+    bool isMagnetized;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMagnetized)
+        {
+            body.velocity = ((posPlayer.position - transform.position) * 2);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+        if (collision.CompareTag("Magnetic"))
+        {
+            posPlayer = collision.transform;
+            isMagnetized = true;
+        }
+        switch (collision.tag)
+        {
+            case "Player":
+                PlayerStats.GainXp(collision.GetComponent<PlayerStats>(), xpGain);
+                Destroy(gameObject);
+                break;
+            case "Magnetic":
+                posPlayer = collision.transform;
+                isMagnetized = true;
+                break;
+        }
+
+    }
+}2
